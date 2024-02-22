abc6642db314e07f3857b051a77eb1c7a6f9d009
Ishita Sharma <isharma2@academic.rrc.ca>
2024-02-22 16:24:38 -0600

    Generated Users modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_204527) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_222346) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_204527) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
