47756bfd13bdabab204fbdc288336d48756c76c1
Ishita Sharma <isharma2@academic.rrc.ca>
2024-02-23 12:50:18 -0600

    updated index and controller file for search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.includes(:genre).page(params[:page]).per(10)
+    @books = @books.where("title LIKE ?", "#{params[:search]}%") if params[:search].present?
   end
 
 
