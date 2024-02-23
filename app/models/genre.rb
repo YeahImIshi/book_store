ad85e4f1d54f626404456802091c593e6f69e3f1
Ishita Sharma <isharma2@academic.rrc.ca>
2024-02-22 20:40:56 -0600

    create about page and updated controller and route

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,11 @@
 class Genre < ApplicationRecord
   has_many :books
+  validates :name, presence: true, length: { maximum: 50 }
+
+  # Validates uniqueness
+  validates :name, uniqueness: true
+
+  # Validates format using a regular expression
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
+
