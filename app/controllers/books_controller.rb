43bdb6cff2afa40b23d791c812b57acad67a75db
Ishita Sharma <isharma2@academic.rrc.ca>
2024-02-23 20:44:41 -0600

    Added heirarchy to search in index and controller file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.includes(:genre).page(params[:page]).per(10)
     @books = @books.where("title LIKE ?", "#{params[:search]}%") if params[:search].present?
+    @books = @books.where(genre_id: params[:genre]) if params[:genre].present?
   end
 
 
